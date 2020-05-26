b4bf6bcb62220be84070482e3be86584715e6ca9
Jason Huang <jason5528460@gmail.com>
2020-05-27 03:13:47 +0800

    Update output to 3 kinds of fmeasure

--- fmeasure.py
+++ fmeasure.py
@@ -5,9 +5,9 @@ def main():
     ref_intervals, ref_pitches = mir.io.load_valued_intervals(sys.argv[1])
     est_intervals, est_pitches = mir.io.load_valued_intervals(sys.argv[2])
     scores = mir.transcription.evaluate(ref_intervals, ref_pitches, est_intervals, est_pitches)
-    print("Precision: " + str(scores['Precision']))
-    print("Recall: " + str(scores['Recall']))
-    print("F-measure: " + str(scores['F-measure']))
+    print("| COn     | " + str(round(scores['Onset_F-measure'], 3)) + ' |')
+    print("| COnP    | " + str(round(scores['F-measure_no_offset'], 3)) + ' |')
+    print("| COnPOff | " + str(round(scores['F-measure'], 3)) + ' |')
 
 if __name__ == "__main__":
     main()
