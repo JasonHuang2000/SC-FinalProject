0cbfb5529a0812b6e995f98c45e5b75c8a92073a
Jason Huang <jason5528460@gmail.com>
2020-05-30 15:15:31 +0800

    Update fmeasure.py

--- fmeasure.py
+++ fmeasure.py
@@ -5,12 +5,13 @@ def main():
     ref_intervals, ref_pitches = mir.io.load_valued_intervals(sys.argv[1])
     est_intervals, est_pitches = mir.io.load_valued_intervals(sys.argv[2])
     scores = mir.transcription.evaluate(ref_intervals, ref_pitches, est_intervals, est_pitches)
-    print("-------------------")
+    print("===================")
     print("| COn     | " + str(round(scores['Onset_F-measure'], 3)) + ' |')
     print("| COnP    | " + str(round(scores['F-measure_no_offset'], 3)) + ' |')
     print("| COnPOff | " + str(round(scores['F-measure'], 3)) + ' |')
     print("-------------------")
     print("Final Score: " + str(round(scores['Onset_F-measure']*0.2 + scores['F-measure_no_offset']*0.6 + scores['F-measure']*0.2, 3)))
+    print("===================")
 
 if __name__ == "__main__":
     main()
